b5d0b6b3c80b1c0b02dc29de362ea1f369289076
wittyZzZ <william.zubarzo@usach.cl>
2024-11-12 01:18:05 -0300

    Se carga proyecto inicial Spring

---
+++ control2/src/main/java/com/tbd_grupo8/control2/Control2Application.java
@@ -0,0 +1,13 @@
+package com.tbd_grupo8.control2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Control2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Control2Application.class, args);
+	}
+
+}
---
+++ control2/src/test/java/com/tbd_grupo8/control2/Control2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbd_grupo8.control2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Control2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
