931e60cc2ab29accf3db673af73b36d30a384ebb
gonzaloArevalo <gonzalo.arevalo@usach.cl>
2024-11-17 10:01:46 -0300

    borrar carpeta frontend y empezar de nuevo

--- frontend_control2/src/plugins/vuetify.js
+++
@@ -1,9 +0,0 @@
-// Styles
-import '@mdi/font/css/materialdesignicons.css';
-import 'vuetify/styles';
-
-// Vuetify
-import { createVuetify } from 'vuetify';
-
-// Crear instancia de Vuetify
-export default createVuetify();
